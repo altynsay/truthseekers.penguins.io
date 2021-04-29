2701e14aa74147be02682cfc7821a151e15a4621
altynsay <74095355+altynsay@users.noreply.github.com>
2021-04-29 18:00:56 +0200

    Update penguins population.js

--- penguins population.js
+++ penguins population.js
@@ -1,7 +1,7 @@
     var yourVlSpec = {
       $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
       description: 'Population of penguins through years',
-      width: 300,
+      width: 400,
       height: 100,
       data: { url: 'penguins count.json'},
       transform: [
